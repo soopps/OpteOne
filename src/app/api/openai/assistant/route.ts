2220de8a01d73089cd6b30df870294ab3f2413aa
soopps <thabang.b.mokwena@gmail.com>
2024-01-24 17:41:46 +0200

    OpteOne - Updated OpenAIAPI - Key

--- src/app/api/openai/assistant/route.ts
+++ src/app/api/openai/assistant/route.ts
@@ -1,8 +1,8 @@
 import OpenAI from "openai";
 import { NextResponse } from "next/server";
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-const openaiModelId:any = process.env.OPENAI_MODEL_ID || null;
+const openai = new OpenAI({ apiKey: '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' });
+const openaiModelId:any = 'gpt-3.5-turbo-16k-0613' || null;
 
 export async function POST(request: Request, context: any) {
     const { params } = context;
